b89303c55ea23331de64b3c9ec5f5d1783215e74
sebastiansimko <sebastian.simko@affinityanalytics.com>
2018-12-28 16:25:43 +0100

    try#2

--- resources/views/emails/forgot-password.blade.php
+++ resources/views/emails/forgot-password.blade.php
@@ -4,9 +4,9 @@
     Ahoj,
     dostali sme požiadavku na resetovanie tvojho hesla ak si želáš pokračovať klikni na
 
-    @component('mail::button', ['url' => $url])
-        Reset Hesla
-    @endcomponent
+@component('mail::button', ['url' => $url, 'color' => 'primary'])
+Reset Hesla
+@endcomponent
 
     Pekný deň praje team,
 
