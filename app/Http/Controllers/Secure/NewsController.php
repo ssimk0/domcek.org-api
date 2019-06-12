213c09a1886711ab69431598626f0d0eefe37e7a
Sebastian Simko <contact@sebastiansimko.sk>
2019-06-12 13:27:05 +0200

    allow more chars to short

--- app/Http/Controllers/Secure/NewsController.php
+++ app/Http/Controllers/Secure/NewsController.php
@@ -22,7 +22,7 @@ class NewsController extends Controller
         $data = $this->validate($request, [
             'title' => 'required|string',
             'body' => 'required|string',
-            'short' => 'required|string|max:200',
+            'short' => 'required|string|max:300',
             'image' => ['required', 'regex:(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})'],
             'status' => 'required|in:draft,archived,published',
             'is_featured' => 'required|boolean'
@@ -44,7 +44,7 @@ class NewsController extends Controller
         $data = $this->validate($request, [
             'title' => 'required|string',
             'body' => 'required|string',
-            'short' => 'required|string',
+            'short' => 'required|string|max:300',
             'image' => ['required', 'regex:(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})'],
             'status' => 'required|in:draft,archived,published',
             'is_featured' => 'required|boolean'
