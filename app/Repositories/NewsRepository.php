7d4957ac92c2bb46f8bcccd232d98520767fd706
sebo <contact@sebastiansimko.sk>
2019-12-05 19:51:27 +0100

    improve logic of news

--- app/Repositories/NewsRepository.php
+++ app/Repositories/NewsRepository.php
@@ -30,12 +30,32 @@ class NewsRepository extends Repository
 
     function findAllLatestFeaturedPublishedNews($size, $offset)
     {
-        return DB::table('news_items')
-            ->where('status', NewsConstant::PUBLISHED)
-            ->where('is_featured', true)
-            ->orderBy('created_at', 'desc')
-            ->skip($offset)
-            ->paginate($size);
+        if ($offset == 0 ) {
+            return DB::table('news_items')
+                ->where('status', NewsConstant::PUBLISHED)
+                ->where('is_featured', true)
+                ->orderBy('created_at', 'desc')
+                ->offset(2)
+                ->paginate($size);
+        } else {
+             $skiped = DB::table('news_items')
+                ->where('status', NewsConstant::PUBLISHED)
+                ->where('is_featured', true)
+                ->orderBy('created_at', 'desc')
+                ->limit($offset)
+                ->get(['id']);
+            $ids = [];
+            foreach ($skiped as $item) {
+                $ids []= $item->id;
+            }
+
+            return DB::table('news_items')
+                ->where('status', NewsConstant::PUBLISHED)
+                ->where('is_featured', true)
+                ->whereNotIn('id', $ids)
+                ->orderBy('created_at', 'desc')
+                ->paginate($size);
+        }
     }
 
     function findNewsDetail($slug)
