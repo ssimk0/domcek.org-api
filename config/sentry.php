ad15e81caeaeb698878a479fe77f267eaae97ef3
sebo <contact@sebastiansimko.sk>
2019-12-11 16:29:33 +0100

    add releases sentry and remove middleware

--- config/sentry.php
+++ config/sentry.php
@@ -4,5 +4,6 @@ return array(
     'dsn' => 'https://2841516f4ef04d8bbd82e9233a0fb6a2@sentry.io/1444639',
 
     // capture release as git sha
-    // 'release' => trim(exec('git log --pretty="%h" -n1 HEAD')),
+    'release' => trim(exec('git log --pretty="%h" -n1 HEAD')),
+    //'release' => 'laravel@1.2.0'
 );
--- routes/web.php
+++ routes/web.php
@@ -28,9 +28,7 @@ $router->group(['prefix' => env('API_PREFIX', '/'), 'middleware' => 'cors'], fun
     // PAGE UNSECURE
     $router->group(
         ['prefix' => '/', 'middleware' => 'cors'], function () use ($router) {
-        $router->group(['prefix' => '/', 'middleware' => 'optimizeImages'], function () use ($router) {
-            $router->post('media/upload', 'Unsecure\MediaController@upload');
-        });
+        $router->post('media/upload', 'Unsecure\MediaController@upload');
         $router->get('pages', 'Unsecure\PageController@menuPages');
         $router->get('pages/{slug}', 'Unsecure\PageController@page');
         $router->get('news', 'Unsecure\NewsController@list');
