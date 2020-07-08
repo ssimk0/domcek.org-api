4d2a7361d55dd85ebd6ac5846d45a844b624cded
sebo <contact@sebastiansimko.sk>
2020-07-08 06:38:18 +0200

    fix email typos

--- resources/views/emails/registrationVolunteer.blade.php
+++ resources/views/emails/registrationVolunteer.blade.php
@@ -3,7 +3,7 @@
 Milý/á {{ $userName }},
 
 prihlásenie na {{ $eventName }} prebehlo úspešne. <br/>
-##Až keď zaplatíš a máš potrvdenú platbu mailom tak si oficialne prihlásený a máš zaistene ubytovanie !!!
+##Až keď zaplatíš a máš potvrdenú platbu mailom tak si oficiálne prihlásený a máš zaistené ubytovanie !!!
 
 @component('mail::panel')
 Údaje pre platbu zálohy:
@@ -23,7 +23,7 @@ Správa pre prijímateľa: Príspevok na činnosť
 <pre style="color:black">
 Zálohu, resp. účastnícky poplatok je potrebné uhradiť najneskôr týždeň pred podujatím. Viac informácií nájdeš vo svojom profile.
 
-Prosím, ulož si do mobilu <strong>QR kod</strong>, ktorý nájdeš v prílohe tohto e-mailu (alebo po prihlásení na stránke <a href="https://domcek.org">www.domcek.org</a>, v sekcii "Moje prihlásenia").
+Prosím, ulož si do mobilu <strong>QR kód</strong>, ktorý nájdeš v prílohe tohto e-mailu (alebo po prihlásení na stránke <a href="https://domcek.org">www.domcek.org</a>, v sekcii "Moje prihlásenia").
 
 A vezmi si so sebou aj <strong>potvrdenie o platbe</strong>  pre prípad, že systém nedokáže spárovať tvoju platbu.
 
@@ -33,7 +33,7 @@ Ak zaplatíš poštovou poukážkou, určite so sebou prines aj ústrižok (stá
 
 V prípade, že chceš zmeniť svoje prihlásenie alebo sa chceš odhlásiť, môžeš tak urobiť na stránke <a href="https://domcek.org">www.domcek.org</a> v sekcii "Moje prihlásenia".
 
-V prípade, že sa z púte odhlásiš v termíne otvoreného prihlasovania, odoslaním mailu na <a href="mailto:pute@domcek.org">pute@domcek.org</a> môžeš požiadať o vrátenie vyplatenej zálohy.
+V prípade, že sa z púte odhlásiš v termíne otvoreného prihlasovania, odoslaním mailu na <a href="mailto:domcek@domcek.org">domcek@domcek.org</a> môžeš požiadať o vrátenie vyplatenej zálohy.
 
 V prípade akýchkoľvek otázok nás môžeš kontaktovať na mailovú adresu <a href="mailto:pute@domcek.org">pute@domcek.org</a> alebo nám napíš na Domčekovkú FB stránku.
 
