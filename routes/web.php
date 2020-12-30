a64e0403c90f925cdc13c6cc1441af8c670e10d7
Sebastian Simko <contact@sebastiansimko.sk>
2020-12-30 09:59:54 +0100

    fix imports

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -30,7 +29,7 @@ $router->group(['prefix' => env('API_PREFIX', '/'), 'middleware' => 'cors'], fun
     $router->group(
         ['prefix' => '/', 'middleware' => 'cors'],
         function () use ($router) {
-            $router->post('media/upload', 'Secure\UploadController@store');
+            $router->post('media/upload', [UploadController::class, 'store']);
             $router->get('pages', 'Unsecure\PageController@menuPages');
             $router->get('pages/{slug}', 'Unsecure\PageController@page');
             $router->get('news', 'Unsecure\NewsController@list');
