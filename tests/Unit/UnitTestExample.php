85cad027598485f3f611212ca94a6474e2843de7
Sebastián Simko <contact@sebastiansimko.sk>
2021-01-29 19:04:54 +0100

    add example unit test

---
+++ tests/Unit/UnitTestExample.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Tests\Unit;
+
+
+use PHPUnit\Framework\TestCase;
+
+class UnitTestExample extends TestCase {
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
